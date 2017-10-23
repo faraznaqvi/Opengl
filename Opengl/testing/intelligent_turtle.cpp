0d8b2fdca309b24f048960a735e0be986ebc11ae
Syed Faraz <faraznaqvi_1994@yahoo.com>
2017-10-23 12:38:10 +0500

    Intelligent Turtle
    This code is an extension of turtle graphics, in the previous turtle it makes lines but it cannot repeat its task. This code provides ability to remember where the turtle was previously be saving the coordinates into the vector list. In this way we can repeat the drawing for endless times.

---
+++ Opengl/testing/intelligent_turtle.cpp
@@ -0,0 +1,124 @@
+#include<stdio.h>
+#include<iostream>
+#include<math.h>
+#include<glut.h>
+#include<vector>// important for using vectors and it is built in...
+using namespace std;
+
+
+int width = 680;
+int height = 640;
+
+class point{
+public: point(int xx, int yy){x=xx;y=yy;}
+		point();
+		int x,y;
+};
+
+vector<point> viki;
+
+class turtle{
+
+public:
+	turtle(point initial){
+		x= initial.x;
+		y = initial.y;
+	}
+	void line_to(int,float);
+	void move_to(int,float);
+	int x,y,x1,y1;
+};
+
+void turtle::line_to(int distance, float angle){
+
+
+	x1 = distance*cos(angle);
+	y1 = distance*sin(angle);
+
+
+	glBegin(GL_LINES);
+		glVertex2f(x,y);
+		glVertex2f(x1,y1);
+	glEnd();
+	glFlush();
+	point p(x,y);
+	viki.push_back(p);
+		x = x1;
+		y = y1;
+}
+
+void turtle::move_to(int distance, float angle){
+	x1 = distance*cos(angle);
+	y1 = distance*sin(angle);
+	x = x1;
+	y = y1;
+}
+
+
+void display(void);
+void draw_vector(point);
+
+
+
+void draw_vector(point p){
+
+
+	glBegin(GL_LINE_STRIP);
+	for(int i =0;i<viki.size();i++){
+		glVertex2f(viki[i].x+p.x,viki[i].y+p.y);
+	}
+	glEnd();
+	glFlush();
+
+
+}
+
+
+void main(int arg, char **argv){
+
+	glutInit(&arg, argv);
+	
+	//setting our display mode and the frame buffer and color settings
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // single buffer and rgb color settings
+	//setting the with and height of the window
+	glutInitWindowSize(width, height);
+	//Now setting our position of the window which will be opened..
+	glutInitWindowPosition(30, 30);
+	//Now giving the title name to the window..
+	glutCreateWindow("farazfirst");
+	
+
+	
+	//Now calling for the display function through call backs
+	glutDisplayFunc(display);
+	glutMainLoop();//main loop 
+}
+
+void display(){
+	gluOrtho2D(-100,100,-100,100);
+	glClearColor(0,0,0,0);//setting the color for the background (Red,Green,Blue,Alpha)
+	//glClear(GL_COLOR_BUFFER_BIT);
+	float distance = 20;
+	glBegin(GL_POINTS);
+		glVertex2f(0,0);
+		glEnd();
+	point p1(0,0);
+	point p2(10,10);
+	point p3(0,-50);
+	point p4(20,-5);
+	turtle t1(p1);
+	t1.line_to(distance,90);
+	t1.line_to(distance,45);
+	t1.move_to(distance,35);
+	t1.line_to(2*distance,25);
+	point p5(10,10);
+	point p6(20,20);
+	
+	draw_vector(p5);
+	draw_vector(p6);
+
+	glColor3f(1.0,1.0,1.0);
+	//glutPostRedisplay();
+	glFlush();
+}
+
